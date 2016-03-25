e2d0b8d8905e93892591627aed291947a6efd7c3
Mike Apostolakis <mr.michaela@gmail.com>
2016-03-25 18:44:23 +0000

    Initial Commit

---
+++ Countries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Countries
+//
+//  Created by Michail G Apostolakis on 25/03/2016.
+//  Copyright © 2016 Mike Apostolakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Countries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Countries
+//
+//  Created by Michail G Apostolakis on 25/03/2016.
+//  Copyright © 2016 Mike Apostolakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
