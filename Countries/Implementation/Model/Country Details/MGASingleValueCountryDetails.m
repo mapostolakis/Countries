915f0ea288a78e15781ecf035ff7ea03426c4454
Mike Apostolakis <mr.michaela@gmail.com>
2016-04-04 00:10:24 +0100

    Remove commented code

--- Countries/Implementation/Model/Country Details/MGASingleValueCountryDetails.m
+++ Countries/Implementation/Model/Country Details/MGASingleValueCountryDetails.m
@@ -4,13 +4,6 @@
 
 #import "MGASingleValueCountryDetails.h"
 
-@interface MGASingleValueCountryDetails ()
-
-//@property (nonatomic, readonly) NSString *name;
-//@property (nonatomic, readonly) NSString *value;
-
-@end
-
 @implementation MGASingleValueCountryDetails
 
 @synthesize name = _name, value = _value;
