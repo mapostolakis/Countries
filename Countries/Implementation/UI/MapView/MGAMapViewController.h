7c4542d3a5d8c573ef5b07b99ff181b27124c37e
Mike Apostolakis <mr.michaela@gmail.com>
2016-04-10 12:03:52 +0100

    Add map view

---
+++ Countries/Implementation/UI/MapView/MGAMapViewController.h
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2016 Mike Apostolakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import MapKit;
+
+@interface MGAMapViewController : UIViewController
+
+@property (nonatomic, weak, readonly) MKMapView *mapView;
+
+- (instancetype)initWithRegion:(MKCoordinateRegion)region;
+
+@end
---
+++ Countries/Implementation/UI/MapView/MGAMapViewController.m
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2016 Mike Apostolakis. All rights reserved.
+//
+
+#import "MGAMapViewController.h"
+
+@interface MGAMapViewController ()
+
+@property (nonatomic, weak, readwrite) MKMapView *mapView;
+@property (nonatomic, readonly) MKCoordinateRegion region;
+
+@end
+
+@implementation MGAMapViewController
+
+- (instancetype)initWithRegion:(MKCoordinateRegion)region
+{
+    self = [super init];
+    if (self) {
+        _region = region;
+    }
+    return self;
+}
+
+- (MKMapView *)mapView
+{
+    if (_mapView == nil) {
+        MKMapView *mapView = [[MKMapView alloc] initWithFrame:CGRectZero];
+        mapView.translatesAutoresizingMaskIntoConstraints = NO;
+        [mapView setRegion:self.region animated:YES];
+        [self.view addSubview:mapView];
+        _mapView = mapView;
+    }
+    return _mapView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [self setupConstraints];
+}
+
+- (void)setupConstraints
+{
+    UIView *mapView = self.mapView;
+
+    NSDictionary *views = NSDictionaryOfVariableBindings(mapView);
+
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[mapView]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[mapView]|" options:0 metrics:nil views:views]];
+}
+
+@end
