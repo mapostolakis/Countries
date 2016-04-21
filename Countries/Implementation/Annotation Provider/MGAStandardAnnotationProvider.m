ecd92ccc02e4e4e0cc2337780f5a5acb201793cf
Mike Apostolakis <mr.michaela@gmail.com>
2016-04-21 13:43:31 +0300

    Replace numberOfObjectsInSection: with numberOfAllObjects

--- Countries/Implementation/Annotation Provider/MGAStandardAnnotationProvider.m
+++ Countries/Implementation/Annotation Provider/MGAStandardAnnotationProvider.m
@@ -27,7 +27,7 @@
 - (NSArray <id <MKAnnotation>> *)createCountryListAnnotations
 {
     NSMutableArray *annotations = [NSMutableArray array];
-    for (NSInteger index=0; index<[self.dataSource numberOfObjectsInSection:0]; index++) {
+    for (NSInteger index=0; index<[self.dataSource numberOfAllObjects]; index++) {
         id <MGACountry> country = [self.dataSource objectAtIndexPath:[NSIndexPath indexPathForRow:index inSection:0]];
         MGACountryAnnotation *annotation = [[[MGACountryAnnotationBuilder alloc] initWithCountry:country] build];
         [annotations addObject:annotation];
